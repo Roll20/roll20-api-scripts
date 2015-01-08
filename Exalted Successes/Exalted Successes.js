5181c8b7f4e2b78c26e57ded70c77c05a3665c07
lithl <blanketyblankman@gmail.com>
2015-01-08 02:05:31 -0600

    Update Exalted successes to v2
    v2 is mostly a refactor

--- Exalted Successes/Exalted Successes.js
+++ Exalted Successes/Exalted Successes.js
@@ -1,65 +1,59 @@
-var exalted = exalted || {};
- 
-exalted.sendChat = function(name, id, msg)
-{
-    var characters = findObjs({_type: 'character'});
-    var speaking;
-    characters.forEach(function(chr) { if(chr.get('name') == name) speaking = chr; });
-    if(speaking) sendChat('character|'+speaking.id, msg);
-    else sendChat('player|'+id, msg);
-};
- 
-on('chat:message', function(msg) {
-    var json;
-    var inline = false;
-    try { json = JSON.parse(msg.content); }
-    catch(e)
-    {
-        if(msg.inlinerolls) inline = true;
-        else return;
-    }
+var bshields = bshields || {};
+bshields.exalted = (function() {
+    'use strict';
     
-    var results = [];
-    if(!inline)
-    {
-        json.rolls.forEach(function(j) {
-            if(j.sides != 10) return;
-            results.push(j.results);
-        });
-    }
-    else
-    {
-        json = msg.inlinerolls;
-        json.forEach(function(j) {
-            var rolls = j.results.rolls;
-            rolls.forEach(function(r) {
-                if(r.sides != 10) return;
-                results.push(r.results);
+    var version = 2.0;
+    
+    function handleInput(msg) {
+        var json = msg.type === 'rollresult' ? JSON.parse(msg.content) : null,
+            inline = !!msg.inlinerolls,
+            results = [],
+            successes = 0,
+            botches = 0;
+        
+        if (json) {
+            _.each(json.rolls, function(roll) {
+                if (roll.sides !== 10) { return; }
+                results.push(roll.results);
+            });
+        } else if (inline) {
+            _.each(msg.inlinerolls, function(rolldata) {
+                _.each(rolldata.results.rolls, function(roll) {
+                    if (roll.sides !== 10) { return; }
+                    results.push(roll.results);
+                });
+            });
+        }
+        
+        _.each(results, function(roll) {
+            _.each(roll, function(die) {
+                var value = die['v'];
+                successes += value >= 7 ? 1 : 0;
+                successes += value === 10 ? 1 : 0;
+                botches += value === 1 ? 1 : 0;
             });
         });
+        
+        if (successes === 0 && botches > 0) {
+            bshields.sendChat(msg, botches + ' botch' + (botches > 1 ? 'es' : ''))
+        } else if (successes === 0) {
+            bshields.sendChat(msg, 'Failure');
+        } else {
+            bshields.sendChat(msg, successes + ' success' + (successes > 1 ? 'es' : ''));
+        }
     }
     
-    var successes = 0;
-    var botches = 0;
-    results.forEach(function(r) {
-        r.forEach(function(d) {
-            var die = d['v'];
-            successes += die >= 7 ? 1 : 0
-            successes += die == 10 ? 1 : 0;
-            botches += die == 1 ? 1 : 0;
-        });
-    });
+    function registerEventHandlers() {
+        on('chat:message', handleInput);
+    }
     
-    if(successes == 0 && botches != 0)
-    {
-        exalted.sendChat(msg.who, msg.playerid, botches+' botch'+(botches>1?'es':''));
-    }
-    else if(successes == 0)
-    {
-        exalted.sendChat(msg.who, msg.playerid, 'Failure');
-    }
-    else
-    {
-        exalted.sendChat(msg.who, msg.playerid, successes+' success'+(successes>1?'es':''));
-    }
+    return {
+        registerEventHandlers: registerEventHandlers
+    };
+}());
+
+on('ready', function() {
+    'use strict';
+    
+    bshields.exalted.registerEventHandlers();
 });
