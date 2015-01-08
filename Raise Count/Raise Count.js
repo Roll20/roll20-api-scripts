6cf95239ce8f2987a375cd9d38d7d55fca498d51
lithl <blanketyblankman@gmail.com>
2015-01-08 02:51:58 -0600

    Update raise count to v2
    v2 is mainly a refactor

--- Raise Count/Raise Count.js
+++ Raise Count/Raise Count.js
@@ -1,65 +1,94 @@
-var raise_count = raise_count || {};
-
-// Raise increment; generalized in case there are cases where it isn't 4
-raise_count.RAISE_SIZE = 4;
-// Output formatting. %1$s will be replaced with an inline roll. %2$s will be replaced by the user's target input.
-// %3$s will be replaced by the number of raises resulting the from roll. change this string if you want the results
-// to show up differently in chat.
-raise_count.OUTPUT_FORMAT = 'Roll: %1$s, Target: %2$s, Raises: %3$s';
-
-on('chat:message', function(msg) {
-    if(msg.type != 'api' || msg.content.indexOf('!rc ')) return;
+var bshields = bshields || {};
+bshields.raiseCount = (function() {
+    'use strict';
     
-    var roll = msg.content.substring(4, msg.content.indexOf('|'));
-    var target = msg.content.substring(msg.content.indexOf('|')+1);
-    
-    var sendAs = 'system';
-    var character = findObjs({_type: 'character', name: msg.who})[0];
-    if(character) sendAs = 'character|'+character.id;
-    else sendAs = 'player|'+msg.playerid;
-    
-    sendChat(sendAs, '[['+roll+']]', function(fmsg) {
-        var expression = fmsg[0].inlinerolls['1'].expression;
-        var total = fmsg[0].inlinerolls['1'].results.total;
-        var raises = Math.floor((total - target) / raise_count.RAISE_SIZE);
-        
-        var rollOut = '<span title="Rolling '+expression+' = ';
-        var fail = crit = false;
-        for(var i in fmsg[0].inlinerolls['1'].results.rolls)
-        {
-            var r = fmsg[0].inlinerolls['1'].results.rolls[i];
-            if(r['type'] != 'R') continue;
-            
-            rollOut += '(';
-            var max = r['sides'];
-            
-            for(var k = 0; k < r['results'].length; k++)
-            {
-                var value = r['results'][k]['v'];
-                crit = crit || (value == max);
-                fail = fail || (value == 1);
-                rollOut += '<span class="basicdiceroll'+(value==max?' critsuccess':(value==1?' critfail':''))+'">';
-                rollOut += value+'</span>+';
+    var version = 2.0,
+        config = {
+            raiseSize: 4,
+            outputFormat: 'Roll: {0}, Target: {1}, Raises: {2}'
+        },
+        commands = {
+            rc: function(args, msg) {
+                var target = parseInt(args.pop(), 10),
+                    roll = args.join(' ');
+                
+                sendChat('', '[[' + roll + ']]', function(ops) {
+                    var expression = ops[0].inlinerolls[1].expression,
+                        total = ops[0].inlinerolls[1].results.total,
+                        raises = Math.floor((total - target) / config.raiseSize),
+                        rollOut = '<span title="Rollin ' + expression + ' = ',
+                        fail = crit = false;
+                    
+                    _.each(ops[0].inlinerolls[1].results.rolls, function(roll) {
+                        var max = roll.sides;
+                        
+                        if (roll.type !== 'R') { return; }
+                        
+                        rollOut += '(';
+                        _.each(roll.results, function(result) {
+                            var value = result.v;
+                            crit = crit || (value === max);
+                            fail = fail || (value === 1);
+                            rollOut += '<span class="basicdiceroll' + (value === max ? ' critsuccess' : (value === 1 ? ' critfail' : '')) + '">';
+                            rollOut += value + '</span>+';
+                        });
+                        rollOut = rollOut.substring(0, rollOut.length - 1) + ')+';
+                    });
+                    
+                    rollOut = rollOut.substr(0, rollOut.length - 1);
+                    rollOut += '" class="a inlinerollresult showtip tipsy-n';
+                    rollOut += (crit && fail ? ' importantroll' : (crit ? ' fullcrit' : (fail ? ' fullfail' : ''))) + '">' + total + '</span>';
+                    
+                    bshields.sendChat(msg, '/direct ' + format(config.outputFormat, rollOut, target, raises));
+                });
             }
-            rollOut = rollOut.substring(0,rollOut.length - 1)+')+';
-        }
-        rollOut = rollOut.substr(0, rollOut.length - 1);
-        rollOut += '" class="a inlinerollresult showtip tipsy-n';
-        rollOut += (crit&&fail?' importantroll':(crit?' fullcrit':(fail?' fullfail':'')))+'">'+total+'</span>';
+        };
+    
+    function format(formatString) {
+        var args = arguments.slice(1);
+        _.each(args, function(arg, index) {
+            formatString = formatString.replace('{' + index + '}', arg);
+        });
+        return formatString;
+    }
+    
+    function handleInput(msg) {
+        var isApi = msg.type === 'api',
+            args = bshields.splitArgs(msg.content.trim()),
+            command, args0, isHelp;
         
-        var message = '/direct '+raise_count.sprintf(raise_count.OUTPUT_FORMAT, rollOut, target, raises);
-        sendChat(sendAs, message);
-    });
-});
+        if (isApi) {
+            command = args.shift().substring(1).toLowerCase();
+            arg0 = args.shift();
+            isHelp = arg0.toLowerCase() === 'help' || arg0.toLowerCase() === 'h';
+            
+            if (!isHelp) {
+                if (arg0 && arg0.length > 0) {
+                    args.unshift(arg0);
+                }
+                
+                if (_.isFunction(commands[command])) {
+                    commands[command](args, msg);
+                }
+            } else if (_.isFunction(commands.help)) {
+                commands.help(command, args, msg);
+            }
+        } else if (_.isFunction(commands['msg_' + msg.type])) {
+            commands['msg_' + msg.type](args, msg);
+        }
+    }
+    
+    function registerEventHandlers() {
+        on('chat:message', handleInput);
+    }
+    
+    return {
+        registerEventHandlers: registerEventHandlers
+    };
+}());
 
-/**
- * Really really really super naive implementation of the sprintf function,
- * which will only really work for this script. I should be ashamed for qriting it.
- */
-raise_count.sprintf = function(format, arg1, arg2, arg3)
-{
-    var out = format.replace('%1$s', arg1);
-    out = out.replace('%2$s', arg2);
-    out = out.replace('%3$s', arg3);
-    return out;
-};
+on('ready', function() {
+    'use strict';
+    
+    bshields.raiseCount.registerEventHandlers();
+});
