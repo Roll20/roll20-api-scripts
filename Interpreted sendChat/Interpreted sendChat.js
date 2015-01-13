3af48d1025079e98deac3bb50d44862521b1ae96
Brian <blanketyblankman@gmail.com>
2015-01-13 10:50:50 -0600

    Update Interpreted sendChat.js

--- Interpreted sendChat/Interpreted sendChat.js
+++ Interpreted sendChat/Interpreted sendChat.js
@@ -20,12 +20,12 @@ var bshields = bshields || {};
 bshields.sendChat = (function() {
     'use strict';
     
-    var version = 2.0;
+    var version = 2.1;
     
     function interpretedSendChat(chatMsg, message) {
         var who = chatMsg.who,
             speaking = _.sortBy(filterObjs(function(obj) { return obj.get('type') === 'character' && obj.get('name').indexOf(who) >= 0; }),
-                                function(chr) { return Math.abs(chr.get('name').toLowerCase().levenshteinDistance(who.toLowerCase()); })[0];
+                                function(chr) { return chr.get('name').toLowerCase().levenshteinDistance(who.toLowerCase()); })[0];
         
         sendChat(speaking ? 'character|' + speaking.id : 'player|' + chatMsg.playerid, message);
     }
