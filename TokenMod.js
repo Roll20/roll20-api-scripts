a9428fe54f859b6cf9eb900d93c06bfcdabd0491
Aaron C. Meadows <shdwjk@gmail.com>
2015-04-25 18:17:14 -0500

    Squashed 'TokenMod/' changes from 2ec3e2e..c151b96
    c151b96 prod-TokenMod: Updated prod version of TokenMod at version 0.8.2.
    
    git-subtree-dir: TokenMod
    git-subtree-split: c151b96870683fb7560f6b829547ead73f56fa6f

--- TokenMod.js
+++ TokenMod.js
@@ -3,11 +3,11 @@
 // Contact:  https://app.roll20.net/users/104025/the-aaron
 
 var TokenMod = TokenMod || (function() {
-	'use strict';
+    'use strict';
 
-	var version = '0.8.1',
-        lastUpdate = 1427607758,
-		schemaVersion = 0.1,
+    var version = '0.8.2',
+        lastUpdate = 1430003592,
+    	schemaVersion = 0.1,
 
 		fields = {
 			// booleans
@@ -145,6 +145,7 @@ var TokenMod = TokenMod || (function() {
 
 
 	showHelp = function(id) {
+        log('here 2');
 		var who=getObj('player',id).get('_displayname').split(' ')[0];
 		sendChat('',
 			'/w '+who+' '
@@ -682,16 +683,16 @@ var TokenMod = TokenMod || (function() {
 					break;
 				case 'toback':
 					break;
-				case 'randomdepth':
-					break;
 
 				case 'player_id':
 					break;
 
 				case 'status':
-					_.each(args, function(a) {
+        			_.each(args, function(a) {
 						var s = a.split(/:/),
-							stat = s.shift(),
+							statparts = s.shift().match((/^(\S+?)(\[(\d*)\]|)$/)),
+                            index = ( '[]' === statparts[2] ? statparts[2] : ( undefined !== statparts[3] ? Math.max(parseInt(statparts[3],10)-1,0) : 0 ) ),
+                            stat=statparts[1],
 							op = (_.contains(['-','+','=','!'],stat[0]) ? stat[0] : false),
 							numraw = s.shift() || '',
 							numop = (_.contains(['-','+'],numraw[0]) ? numraw[0] : false),
@@ -703,6 +704,7 @@ var TokenMod = TokenMod || (function() {
 							retr[cmd].push({
 								status: stat,
 								number: num,
+                                index: index,
 								sign: numop,
 								operation: op || '+'
 							});
@@ -761,16 +763,46 @@ var TokenMod = TokenMod || (function() {
 			},base)
 			.value();
 	},
+    
+    decomposeStatuses = function(statuses){
+        return _.reduce(statuses.split(/,/).reverse(),function(memo,st,idx){
+            var parts=st.split(/@/),
+            entry = {
+                mark: parts[0],
+                num: parseInt(parts[1],10) || 0,
+                idx: idx
+            };
+            if(parts[0].length) {
+                memo[parts[0]] = ( memo[parts[0]] && memo[parts[0]].push(entry) && memo[parts[0]]) || [entry] ;
+            }
+            return memo;
+        },{});
+    },
+
+    composeStatuses = function(statuses){
+        return _.chain(statuses)
+            .reduce(function(m,s){
+                _.each(s,function(sd){
+                    m.push(sd);
+                });
+                return m;
+            },[])
+            .sortBy(function(s){
+                return s.idx;
+            })
+            .map(function(s){
+                return ('dead'===s.mark ? 'dead' : ( s.mark+(s.num>0 ? '@'+s.num : '')));
+            })
+            .value()
+            .reverse()
+            .join(',');
+    },    
+    
 	applyModListToToken = function(modlist, token) {
 		var mods={},
 			delta, cid,
-			current=_.reduce(token.get('statusmarkers').split(/,/),function(memo,st){
-				var parts=st.split(/@/);
-				if(parts[0].length) {
-					memo[parts[0]]=parseInt(parts[1],10)||0;
-				}
-				return memo;
-			},{});
+			current=decomposeStatuses(token.get('statusmarkers')),
+            statusCount=token.get('statusmarkers').split(/,/).length;
 
 		_.each(modlist.on,function(f){
 			mods[f]=true;
@@ -787,21 +819,52 @@ var TokenMod = TokenMod || (function() {
 					_.each(f, function (sm){
 						switch(sm.operation){
 							case '!':
-								if(_.has(current,sm.status)){
-									current = _.omit(current,sm.status);
+								if('[]' !== sm.index && _.has(current,sm.status) ){
+                                    if( _.has(current[sm.status],sm.index) ) {
+                                        current[sm.status]= _.filter(current[sm.status],function(e,idx){
+                                            return idx !== sm.index;
+                                        });
+                                    }
 								} else {
-									current[sm.status] = Math.max(0,Math.min(9,getRelativeChange(current[sm.status], sm.sign+sm.number)));
+                                    current[sm.status] = current[sm.status] || [];
+									current[sm.status].push({
+                                        mark: sm.status,
+                                        num: Math.max(0,Math.min(9,getRelativeChange(0, sm.sign+sm.number))),
+                                        index: statusCount++
+                                    });
 								}
 								break;
 							case '+':
-								current[sm.status] = Math.max(0,Math.min(9,getRelativeChange(current[sm.status], sm.sign+sm.number)));
+								if('[]' !== sm.index && _.has(current,sm.status) && _.has(current[sm.status],sm.index)){
+                                    current[sm.status][sm.index].num = (Math.max(0,Math.min(9,getRelativeChange(current[sm.status][sm.index].num, sm.sign+sm.number))));
+                                } else {
+                                    current[sm.status] = current[sm.status] || [];
+									current[sm.status].push({
+                                        mark: sm.status,
+                                        num: Math.max(0,Math.min(9,getRelativeChange(0, sm.sign+sm.number))),
+                                        index: statusCount++
+                                    });
+                                }
 								break;
 							case '-':
-								current = _.omit(current,sm.status);
+								if('[]' !== sm.index && _.has(current,sm.status)){
+                                    if( _.has(current[sm.status],sm.index )) {
+                                        current[sm.status]= _.filter(current[sm.status],function(e,idx){
+                                            return idx !== sm.index;
+                                        });
+                                    }
+                                } else {
+									current[sm.status]= _.first(current[sm.status],-1);
+                                }
 								break;
 							case '=':
 								current = {};
-								current[sm.status] = Math.max(0,Math.min(9,getRelativeChange(current[sm.status], sm.sign+sm.number)));
+                                current[sm.status] = [];
+                                current[sm.status].push({
+                                    mark: sm.status,
+                                    num: Math.max(0,Math.min(9,getRelativeChange(0, sm.sign+sm.number))),
+                                    index: statusCount++
+                                });
 								break;
 						}
 					});
@@ -879,9 +942,10 @@ var TokenMod = TokenMod || (function() {
 					break;
 			}
 		});
-		mods.statusmarkers=_.map(current,function(v,k){ return ('dead' === k ? k : k+'@'+v); }).join(',');
+		mods.statusmarkers=composeStatuses(current);
 		token.set(mods);
 	},
+
 
 	handleConfig = function(config, id) {
 		var args, cmd, who=getObj('player',id).get('_displayname').split(' ')[0];
@@ -962,6 +1026,7 @@ var TokenMod = TokenMod || (function() {
 					cmds=args.shift().match(/([^\s]+\|'[^']+'|[^\s]+\|"[^"]+"|[^\s]+)/g);
 					switch(cmds.shift()) {
 						case 'help':
+                            log('here');
 							showHelp(msg.playerid);
 							return;
 
