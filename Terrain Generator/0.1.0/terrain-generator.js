259a3506d78c0429acbeffa5d74e21fc3b6fb681
Sebastian Onycher <onycher@gmail.com>
2017-01-29 10:06:11 +0100

    Bump up version

--- Terrain Generator/0.1.0/terrain-generator.js
+++ Terrain Generator/0.1.0/terrain-generator.js
@@ -2,7 +2,7 @@ var TerrainGenerator = TerrainGenerator || (function () {
         'use strict';
         const metadata = {
             name: 'TerrainGenerator',
-            version: '0.1.0'
+            version: '0.2.0'
         };
 
         function getWhisperTarget(id) {
