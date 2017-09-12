8d96831145f0d5a7f99fceafb299258da7246efd
John D <teen120@users.noreply.github.com>
2017-09-12 21:49:41 +0700

    Ensure only run by GM

--- SR-NextPass/0.1/SR-NextPass.js
+++ SR-NextPass/0.1/SR-NextPass.js
@@ -1,13 +1,11 @@
 on('chat:message', function(msg) {
-    if(msg.content == '!nextpass') {
-        //TODO: ensure only run by GM?
-        if(Campaign().get("turnorder") == "") {
-            turnorder = [];
-        } else {
-            turnorder = JSON.parse(Campaign().get("turnorder"));
-        }
+    if( msg.type==='api' 
+        && msg.content.match(/^!nextpass/) 
+        && playerIsGM(msg.playerid) //< ensure only run by GM
+    ) {
+        let turnorder = JSON.parse(Campaign().get('turnorder'))||[];
+        let nextPassOrder = [];
 
-        nextPassOrder = [];
         //foreach obj in turnorder
         for(i = 0; i < turnorder.length; i++) {
             //if pr < 10 remove from []
@@ -15,11 +13,12 @@ on('chat:message', function(msg) {
             if(turnorder[i].pr > 10) {
                 turnorder[i].pr = turnorder[i].pr - 10;
                 nextPassOrder.push(turnorder[i]);
+            } else if(turnorder[i].pr<0){
+                nextPassOrder.push(turnorder[i]);
             }
         }
         nextPassOrder.sort(function(a, b) { return b.pr-a.pr; });
         //sort array desc
         Campaign().set("turnorder", JSON.stringify(nextPassOrder));
     }
-
 });
