f12dd273afd3706b831f18d33a2cb6419a01eb5d
Robin Kuiper <robingjkuiper@gmail.com>
2018-05-06 00:59:48 +0200

    Version

--- 5eTreasure/0.1.5/Treasure.js
+++ 5eTreasure/0.1.5/Treasure.js
@@ -1255,7 +1255,7 @@ var Treasure = Treasure || (function() {
 
             target = (state[state_name].config.chat_magic_items === 'gm') ? 'gm' : '';
             if(target === 'gm') magic_contents += makeButton('Show', '!'+state[state_name].config.command+' show magic', styles.button + styles.float.right);
-            makeAndSendMenu(magic_contents, 'Treasure', target);
+            if(magic_contents) makeAndSendMenu(magic_contents, 'Treasure', target);
         }
     },
 
