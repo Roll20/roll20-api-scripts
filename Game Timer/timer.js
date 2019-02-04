c9ec4ac83697cdef53d7efdf7313aedbd1959396
zeteticl <cougar.l@hotmail.com>
2019-02-04 13:02:50 +0800

    Update timer.js

--- Game Timer/timer.js
+++ Game Timer/timer.js
@@ -1,92 +1,68 @@
-
 on("ready", function () {
-    //Wait until the ready event fires so we know the game is completely loaded.
-    //Get a reference to our patrolling token.
-    // Begins
-    start();
-});
 
-function start() {
-    setTimeout(function () {
-        for (i = 0; i < findObjs({ _type: "graphic", name: "game.clock" }, { caseInsensitive: true }).length; i++) {
-            var obj = findObjs({ _type: "graphic", name: "game.clock" }, { caseInsensitive: true })[i];
-            //We know there is a token in the Game called "Guard A".
-            if (obj != undefined) {
-                //log(obj);
-                //{"_id":"-","_pageid":"-","left":735.1025728734535,"top":385.10257287345354,"width":70,"height":70,"rotation":66,"layer":"objects","isdrawing":false,"flipv":false,"fliph":false,"imgsrc":"/images/character.png","name":"Game.clocK","gmnotes":"","controlledby":"","bar1_value":"1","bar1_max":"","bar1_link":"","bar2_value":"","bar2_max":"","bar2_link":"","bar3_value":"3","bar3_max":"","bar3_link":"","represents":"-","aura1_radius":"","aura1_color":"#FFFF99","aura1_square":false,"aura2_radius":"","aura2_color":"#59E594","aura2_square":false,"tint_color":"transparent","statusmarkers":"","showname":false,"showplayers_name":false,"showplayers_bar1":false,"showplayers_bar2":false,"showplayers_bar3":false,"showplayers_aura1":false,"showplayers_aura2":false,"playersedit_name":true,"playersedit_bar1":true,"playersedit_bar2":true,"playersedit_bar3":true,"playersedit_aura1":true,"playersedit_aura2":true,"light_radius":"","light_dimradius":"","light_otherplayers":false,"light_hassight":false,"light_angle":"","light_losangle":"","light_multiplier":1,"adv_fow_view_distance":"","sides":"","currentSide":0,"lastmove":"735,245","_type":"graphic","_subtype":"token","_cardid":""}
-                //log(obj.get("rotation"));
-                if (obj.get("rotation") >= 360) {
-                    obj.set("rotation", obj.get("rotation") - 360);
+    let lastTime = Date.now();
+    let lastSecond = Math.floor(lastTime / 1000) * 1000;
+    let rotPerSec = 360 / 60;
+    const updateSpeed = 250;
+
+    const statusLookup = [
+        "red",
+        "red,blue",
+        "red,blue,green",
+        "red,blue,green,brown",
+        "red,blue,green,brown,purple",
+        "red,blue,green,brown,purple,yellow",
+        "red,blue,green,brown,purple",
+        "red,blue,green,brown",
+        "red,blue,green",
+        "red,blue"
+    ];
+
+
+    const doUpdates = () => {
+        let nextSecond = false;
+        let time = Date.now();
+        let deltaS = time - lastSecond;
+        lastTime = time;
+
+        findObjs({
+            type: 'graphic',
+            gmnotes: 'game.clock'
+        }, { caseInsensitive: true })
+            .forEach((obj) => {
+                let c = {
+                    bar3_value: obj.get('bar3_value') || "0",
+                    bar1_value: obj.get('bar1_value') || "0",
+                    bar2_value: obj.get('bar2_value') || "0",
+                    rotation: ((((parseInt(obj.get('bar2_value')) || 0) * rotPerSec) + (rotPerSec * (deltaS / 1000)) % 360 + 360) % 360) + (parseInt(obj.get('light_losangle')) || 0)
+                };
+                if (deltaS > 1000) {
+                    let h = parseInt(c.bar3_value);
+                    let m = parseInt(c.bar1_value);
+                    let s = parseInt(c.bar2_value);
+                    s += 1;
+                    m += (s >= 60 ? 1 : 0);
+                    s %= 60;
+                    h += (m >= 60 ? 1 : 0);
+                    m %= 60;
+                    c.bar3_value = `${h}`;
+                    //c.bar1_value = `${m}`;
+                    //c.bar2_value = `${s}`;
+                    c.bar1_value = (`${m}` < 10) ? '0' + `${m}` : `${m}`;
+                    c.bar2_value = (`${s}` < 10) ? '0' + `${s}` : `${s}`;
+                    c.name = c.bar3_value + ':' + c.bar1_value + ':' + c.bar2_value;
+                    let phase = (s % (statusLookup.length));
+                    c.statusmarkers = statusLookup[phase];
+                    nextSecond = true;
                 }
-                obj.set("rotation", obj.get("rotation") + 360 / 60);
-                if (obj.get("bar3_value") == "") obj.set("bar3_value", "0");
-                if (obj.get("bar1_value") == "") obj.set("bar1_value", "0");
-                if (obj.get("bar2_value") == "") obj.set("bar2_value", "0");
-                if (obj.get("gmnotes") == "") obj.set("gmnotes", "1");
-                //statusmarkers "red", "blue", "green", "brown", "purple",
-                switch (obj.get("gmnotes")) {
-                    case "1":
-                        obj.set("statusmarkers", "red");
-                        obj.set("gmnotes", "2");
-                        break;
-                    case "2":
-                        obj.set("statusmarkers", "red,blue");
-                        obj.set("gmnotes", "3");
-                        break;
-                    case "3":
-                        obj.set("statusmarkers", "red,blue,green");
-                        obj.set("gmnotes", "4");
-                        break;
-                    case "4":
-                        obj.set("statusmarkers", "red,blue,green,brown");
-                        obj.set("gmnotes", "5");
-                        break;
-                    case "5":
-                        obj.set("statusmarkers", "red,blue,green,brown,purple");
-                        obj.set("gmnotes", "6");
-                        break;
-                    case "6":
-                        obj.set("statusmarkers", "red,blue,green,brown,purple,yellow");
-                        obj.set("gmnotes", "7");
-                        break;
-                    case "7":
-                        obj.set("statusmarkers", "red,blue,green,brown,purple");
-                        obj.set("gmnotes", "8");
-                        break;
-                    case "8":
-                        obj.set("statusmarkers", "red,blue,green,brown");
-                        obj.set("gmnotes", "9");
-                        break;
-                    case "9":
-                        obj.set("statusmarkers", "red,blue,green");
-                        obj.set("gmnotes", "10");
-                        break;
-                    case "10":
-                        obj.set("statusmarkers", "red,blue");
-                        obj.set("gmnotes", "1");
-                        break;
-                    default:
-                        break;
-                }
-                if (obj.get("bar2_value") == "59") {
-                    obj.set("bar2_value", "0")
-                    if (obj.get("bar1_value") == "59") {
-                        obj.set("bar1_value", "0")
-                        obj.set("bar3_value", parseInt(obj.get("bar3_value"), 10) + 1)
-                    }
-                    else {
-                        obj.set("bar1_value", parseInt(obj.get("bar1_value"), 10) + 1);
-                    }
-                }
-                else {
-                    obj.set("bar2_value", parseInt(obj.get("bar2_value"), 10) + 1);
-                }
-            }
-            else {
-                //log("Not Find")
-            }
+                obj.set(c);
+            });
+        if (nextSecond) {
+            lastSecond += 1000;
         }
-        // Again
-        start();
-    }, 1000);        // Every 1 sec
-}
+    };
+    setInterval(
+        doUpdates,
+        updateSpeed
+    );
+});
