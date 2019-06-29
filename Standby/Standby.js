2f715200507bf3f2b94c52cdf9e1656effb55308
VoltCruelerz <mgreene11trigon@yahoo.com>
2019-06-29 12:37:31 -0400

    working commit

--- Standby/Standby.js
+++ Standby/Standby.js
@@ -1,9 +1,16 @@
-
+if (MarkStart) MarkStart('Standby');
 on('ready', () => {
-    const mcname = 'Standby';
+    const sname = 'Standby';
     const StatusPrefix = 'status_';
     const StandbyIcon = 'interdiction';
     const v = 0.1;
+
+    class Status {
+        constructor(type, value) {
+            this.Type = type;
+            this.Value = value;
+        }
+    }
 
     const GetStatuses = (token) => {
         const rawStatus = token.get('statusmarkers');
@@ -90,114 +97,178 @@ on('ready', () => {
     on('chat:message', (msg) => {
         if (msg.type !== 'api') return;
         if (msg.content.startsWith('!standby') !== true) return;
-        const isYank = msg.content.endsWith('yank');
-        if(Campaign().get('turnorder') === '') {
-            Campaign().set('turnorder','[]');
+        if (!msg.selected) {
+            sendChat(sname, '/w gm Error: You must have tokens selected to use this script.');
+            return;
         }
-        const oldOrder = JSON.parse(Campaign().get('turnorder'));
+        const isYank = msg.content.endsWith('yank');
+        const oldOrderStr = Campaign().get('turnorder');
+        if(oldOrderStr === '' || oldOrderStr === '[]') {
+            sendChat(sname, '/w gm Error: No initiative is loaded.');
+            return;
+        }
+        log('Old Turn Order: ' + oldOrderStr);
+        const oldOrder = JSON.parse(oldOrderStr);
         const yankedIds = {};
+        let yankCount = 0;
         const restoredObjs = [];// NOTE: These are not yet in the string format required
         msg.selected.forEach((selection) => {
+            log('Standby Processing of ' + selection._id);
             let token = getObj('graphic', selection._id);
 
             // Yank it from initiative order and write
             if (isYank) {
+                log('  Yank');
                 let pr = -1;
                 for (let i = 0; i < oldOrder.length; i++) {
                     const entry = oldOrder[i];
-                    if (entry['id'] === token._id) {
-                        pr = parseFloat(entry['pr']);
-                        yankedIds[entry['id']] = true;
+                    log('    Considering: ' + JSON.stringify(entry));
+                    if (entry.id === selection._id) {
+                        log('    Found yankable.');
+                        pr = parseFloat(entry.pr);
+                        yankedIds[entry.id] = true;
+                        yankCount++;
                         break;
                     }
                 }
+                // Don't do anything if it wasn't in init order
+                if (pr === -1) {
+                    log('    Searched, but could not find.');
+                    return;
+                }
+                log('    PR: ' + pr);
                 const noDecimal = 100*pr;
-                //token.set(StandbyIcon, "" + noDecimal);
+                log('    No Decimal: ' + noDecimal);
+                token.set(StandbyIcon, "" + noDecimal);
                 UpdateStatusValue(token, StandbyIcon, noDecimal);
+                log('    Updated token status.');
             }
             // Add back to initiative order and remove status value
             else {
-                const initVal = GetStatusValue(token, StandbyIcon) / 100;
+                log('  Restore');
+                const statusVal = GetStatusValue(token, StandbyIcon);
+                if (isNaN(statusVal)) {
+                    return;
+                }
+                const initVal = statusVal / 100;
+                log('    Init Val: ' + initVal);
                 restoredObjs.push({
                     id: selection._id,
                     pr: initVal
                 });
+                token.set(StandbyIcon, false);
+                UpdateStatusValue(token, StandbyIcon, false);
+                log('    Updated token status.');
             }
         });
-        
-        const newOrder = [];
 
         // Yank out the chosen ids
         if (isYank) {
+            if (yankCount === 0) {
+                sendChat(sname, '/w gm Error: no yankable tokens selected.');
+                return;
+            }
+            const newOrder = [];
+
+            log('Yank Assembly');
             for (let i = 0; i < oldOrder.length; i++) {
                 const entry = oldOrder[i];
-                if (!yankedIds[entry['id']]) {
+                if (!yankedIds[entry.id]) {
                     newOrder.push(entry);
                 }
             }
+            log('  New Order: ' + newOrder);
+
+            // Set
+            Campaign().set('turnorder', JSON.stringify(newOrder));
         }
         // Insert the chosen ids, accounting for the circular nature of initiative
         else {
+            if (restoredObjs.length === 0) {
+                sendChat(sname, '/w gm Error: no restorable tokens selected.');
+                return;
+            }
+            log('Restore Assembly');
             // Sort the restored list
             restoredObjs.sort((a, b) => (a.pr > b.pr ? -1 : 1));
+            log('  Restored Sorted: ' + JSON.stringify(restoredObjs));
 
             // Get the location of the maximum so we know the offset in the future.
-            let maxIndex = -1;
-            let maxId = '';
             let maxVal = -2;
             const sortableOld = [];
+            let oldStarterPR = 0;
             for (let i = 0; i < oldOrder.length; i++) {
                 const entry = oldOrder[i];
-                const val = parseFloat(entry['pr']);
+                const val = parseFloat(entry.pr);
+                sortableOld.push({
+                    id: entry.id,
+                    pr: val
+                })
+                if (i === 0) {
+                    oldStarterPR = entry.id;
+                }
                 if (val > maxVal) {
                     maxVal = val;
-                    maxId = entry['id'];
-                    maxIndex = i;
-                    sortableOld.push({
-                        id: entry['id'],
-                        pr: val
-                    })
+                    log('Max Id: ' + entry.id);
+                    log('Max Val: ' + val);
                 }
             }
+            log('  Presorted Old Order: ' + JSON.stringify(sortableOld));
             sortableOld.sort((a, b) => (a.pr > b.pr ? -1 : 1));
+            log('  Sorted Old Order: ' + JSON.stringify(sortableOld));
 
             // Merge the lists into one sorted list
             let oldIndex = 0;
             let newIndex = 0;
             const merged = [];
-            for (let i = 0; i < newOrder.length + sortableOld.length; i++){
-                oldEntry = sortableOld[oldIndex];
-                newEntry = newOrder[newIndex];
+            log('Old Count: ' + sortableOld.length);
+            log('New Count: ' + restoredObjs.length);
+            for (let i = 0; i < restoredObjs.length + sortableOld.length; i++){
+                if (oldIndex === sortableOld.length) {
+                    log('    Overflow so Push New');
+                    merged.push(restoredObjs[newIndex]);
+                    newIndex++;
+                    continue;
+                } else if (newIndex === restoredObjs.length) {
+                    log('    Overflow so Push Old');
+                    merged.push(sortableOld[oldIndex]);
+                    oldIndex++;
+                    continue;
+                }
+
+                let oldEntry = sortableOld[oldIndex];
+                log('    Comparing[' + oldIndex + ']: ' + JSON.stringify(oldEntry));
+                let newEntry = restoredObjs[newIndex];
+                log('         VS. [' + newIndex + ']: ' + JSON.stringify(newEntry));
                 if (oldEntry.pr >= newEntry.pr) {
+                    log('    Push Old');
                     merged.push(oldEntry);
                     oldIndex++;
                 }
                 if (oldEntry.pr <= newEntry.pr) {
+                    log('    Push New');
                     merged.push(newEntry);
                     newIndex++;
                 }
 
                 // If there was a tie, increment i an extra time so we don't overrun the end
                 if (oldEntry.pr === newEntry.pr) {
+                    log('    Hard Increment i');
                     i++;
                 }
             }
+            log('  Merged Order: ' + JSON.stringify(merged));
 
             // Rotate array
-            while (merged.length > 0 && merged[0].id !== maxId) {
+            while (merged[0].id !== oldStarterPR) {
                 merged.push(merged.shift());
+                log('  Rotated Merged: ' + JSON.stringify(merged));
             }
 
-            // Convert to Roll20 version
-            for (let i = 0; i < merged.length; i++) {
-                newOrder.push({
-                    'id': '' + merged[i].id,
-                    'pr': '' + merged[i].pr
-                });
-            }
+            // Set
+            Campaign().set('turnorder', JSON.stringify(merged));
+            sendChat(sname, '/w gm Turn Order restored.');
         }
-
-        // Set
-        Campaign().set('turnorder', JSON.stringify(newOrder));
     });
 });
+if (MarkStop) MarkStop('Standby');
