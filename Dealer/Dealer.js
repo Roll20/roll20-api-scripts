675043562e377cba736edfca7f4aef7c6992993a
keithcurtis1 <keithcurtis@gmail.com>
2019-08-19 07:24:52 -0700

    Update Dealer.js

--- Dealer/Dealer.js
+++ Dealer/Dealer.js
@@ -1,13 +1,14 @@
-// Dealer
+on('ready',()=>{
+    // Dealer
 // Last Updated: 2019-08-16
 // A script to deal and take cards to selected users from specified decks.
 // Syntax is !deal --[give,take] [number of cards as integer] --[deck name]
 
 log('-=> Dealer v' + version + ' <=-');
+const version = '1.0.0';
 
 on('chat:message', (msg) => {
     if ('api' === msg.type && /!deal\b/i.test(msg.content) && msg.selected) {
-const version = '1.0.0';
 
 
         //get parameter and use default of 'give' if parameter is missing or malformed
@@ -128,4 +129,5 @@ const version = '1.0.0';
         }
         while (numCards > 0);
     }
+});
 });
