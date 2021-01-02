46d88d956adb8cf21373d69ac7ca1231c2d0ffd9
Chris Duncan <chris@theduncans.us>
2021-01-02 04:55:57 -0800

    Simplify arg names since they are always objects.

--- Cypher Systems by Roll20 Companion/1.1.0/cypher_systems_by_roll20.js
+++ Cypher Systems by Roll20 Companion/1.1.0/cypher_systems_by_roll20.js
@@ -14,10 +14,10 @@ const CypherSystemsByRoll20 = (function () {
 ===========================================================================`)
   }
 
-  function modStat (characterObj, statName, statCost) {
+  function modStat (character, statName, statCost) {
     // checking the stat
     if (statName !== 'might' && statName !== 'speed' && statName !== 'intellect' && statName !== 'recovery-rolls') {
-      sendChat(`character|${characterObj.id}`, `&{template:default} {{modStat=1}} {{noAttribute=${statName}}}`)
+      sendChat(`character|${character.id}`, `&{template:default} {{modStat=1}} {{noAttribute=${statName}}}`)
       return
     }
     let obj1
@@ -26,18 +26,18 @@ const CypherSystemsByRoll20 = (function () {
       const objArray = findObjs({
         _type: 'attribute',
         name: stat1,
-        _characterid: characterObj.id
+        _characterid: character.id
       })
       if (!objArray.length) {
         obj1 = createObj('attribute', {
           name: stat1,
           current: statCost,
-          characterid: characterObj.id
+          characterid: character.id
         })
       } else {
         objArray[0].setWithWorker('current', statCost)
       }
-      sendChat(`character|${characterObj.id}`, 'Next recovery period updated.')
+      sendChat(`character|${character.id}`, 'Next recovery period updated.')
     } else {
       // stat pool modification
       let pool1 = 0
@@ -46,16 +46,16 @@ const CypherSystemsByRoll20 = (function () {
       let objArray = findObjs({
         _type: 'attribute',
         name: stat1,
-        _characterid: characterObj.id
+        _characterid: character.id
       })
       if (!objArray.length) {
-        pool1 = parseInt(getAttrByName(characterObj.id, stat1, 'current')) || 0
-        max1 = parseInt(getAttrByName(characterObj.id, stat1, 'max')) || 0
+        pool1 = parseInt(getAttrByName(character.id, stat1, 'current')) || 0
+        max1 = parseInt(getAttrByName(character.id, stat1, 'max')) || 0
         obj1 = createObj('attribute', {
           name: stat1,
           current: pool1,
           max: max1,
-          characterid: characterObj.id
+          characterid: character.id
         })
       } else {
         obj1 = objArray[0]
@@ -88,16 +88,16 @@ const CypherSystemsByRoll20 = (function () {
         objArray = findObjs({
           _type: 'attribute',
           name: stat2,
-          _characterid: characterObj.id
+          _characterid: character.id
         })
         if (!objArray.length) {
-          pool2 = parseInt(getAttrByName(characterObj.id, stat2, 'current')) || 0
-          max2 = parseInt(getAttrByName(characterObj.id, stat2, 'max')) || 0
+          pool2 = parseInt(getAttrByName(character.id, stat2, 'current')) || 0
+          max2 = parseInt(getAttrByName(character.id, stat2, 'max')) || 0
           obj2 = createObj('attribute', {
             name: stat2,
             current: pool2,
             max: max2,
-            characterid: characterObj.id
+            characterid: character.id
           })
         } else {
           obj2 = objArray[0]
@@ -106,16 +106,16 @@ const CypherSystemsByRoll20 = (function () {
         objArray = findObjs({
           _type: 'attribute',
           name: stat3,
-          _characterid: characterObj.id
+          _characterid: character.id
         })
         if (!objArray.length) {
-          pool3 = parseInt(getAttrByName(characterObj.id, stat3, 'current')) || 0
-          max3 = parseInt(getAttrByName(characterObj.id, stat3, 'max')) || 0
+          pool3 = parseInt(getAttrByName(character.id, stat3, 'current')) || 0
+          max3 = parseInt(getAttrByName(character.id, stat3, 'max')) || 0
           obj3 = createObj('attribute', {
             name: stat3,
             current: pool3,
             max: max3,
-            characterid: characterObj.id
+            characterid: character.id
           })
         } else {
           obj3 = objArray[0]
@@ -129,27 +129,27 @@ const CypherSystemsByRoll20 = (function () {
           obj2.setWithWorker('current', 0)
           if (statCost > pool3) {
             obj3.setWithWorker('current', 0)
-            sendChat(`character|${characterObj.id}`, `He's dead, Jim! ${pool1}, ${pool2}, and ${pool3} down to 0.`)
+            sendChat(`character|${character.id}`, `He's dead, Jim! ${pool1}, ${pool2}, and ${pool3} down to 0.`)
           } else {
             finalPool = pool3 - statCost
             obj3.setWithWorker('current', finalPool)
-            sendChat(`character|${characterObj.id}`, `${stat1} and ${stat2} down to 0. ${stat3}: ${pool3}-${statCost}=${finalPool}`)
+            sendChat(`character|${character.id}`, `${stat1} and ${stat2} down to 0. ${stat3}: ${pool3}-${statCost}=${finalPool}`)
           }
         } else {
           finalPool = pool2 - statCost
           obj2.setWithWorker('current', finalPool)
-          sendChat(`character|${characterObj.id}`, `${stat1} down to 0. ${stat2}: ${pool2}-${statCost}=${finalPool}`)
+          sendChat(`character|${character.id}`, `${stat1} down to 0. ${stat2}: ${pool2}-${statCost}=${finalPool}`)
         }
       } else {
         // just the current stat is diminished
         finalPool = pool1 - statCost
         obj1.setWithWorker('current', finalPool)
-        sendChat(`character|${characterObj.id}`, `${stat1}: ${pool1}-${statCost}=${finalPool}`)
+        sendChat(`character|${character.id}`, `${stat1}: ${pool1}-${statCost}=${finalPool}`)
       }
     }
   }
 
-  function npcDamage (tokenObj, characterObj, dmgDealt, applyArmor) {
+  function npcDamage (token, character, dmgDealt, applyArmor) {
     // Apply damage (or healing if dmgDealt is negative ...) to Numenera NPC/Creature
     // And set 'death' marker if health is 0 or less.
     // The Mook or Non Player full Character must have the following attributes :
@@ -158,28 +158,28 @@ const CypherSystemsByRoll20 = (function () {
     //  - Armor (token bar3)
     // Armor will diminish damage unless 'applyArmor'='n'
     let dmg = parseInt(dmgDealt) || 0
-    const npcName = characterObj.get('name')
+    const npcName = character.get('name')
     let npcHealth = 0
     let npcMaxHealth = 0
     let npcArmor = 0
     let attObjArray = {}
     if (applyArmor !== 'n') {
-      npcArmor = parseInt(getAttrByName(characterObj.id, 'armor', 'current')) || 0
+      npcArmor = parseInt(getAttrByName(character.id, 'armor', 'current')) || 0
       // DEBUG
       // sendChat('GM', '/w gm npcDamage() Debug : Armor of (''+npcName+'', char id:'+characterObj.id+', token id:'+tokenObj.id+') = '+npcArmor)
     }
     // Is the token linked to the character ('full NPC') or a Mook ?
-    const isChar = tokenObj.get('bar1_link')
+    const isChar = token.get('bar1_link')
     if (isChar === '') {
       // It's a Mook : get the bars value
-      npcHealth = parseInt(tokenObj.get('bar2_value'))
-      npcMaxHealth = parseInt(tokenObj.get('bar2_max'))
+      npcHealth = parseInt(token.get('bar2_value'))
+      npcMaxHealth = parseInt(token.get('bar2_max'))
     } else {
       // It's a 'full' character NPC : get the attributes values
       attObjArray = findObjs({
         _type: 'attribute',
         name: 'health',
-        _characterid: characterObj.id
+        _characterid: character.id
       })
       if (attObjArray === false) {
         sendChat('GM', `/w gm npcDamage() Error: ${npcName} has no health attribute!`)
@@ -197,14 +197,14 @@ const CypherSystemsByRoll20 = (function () {
     const npcHealthFinal = Math.min(Math.max((npcHealth - dmg), 0), npcMaxHealth)
     if (isChar === '') {
       // Mook : update bars onbly
-      tokenObj.set('bar2_max', npcMaxHealth)
-      tokenObj.set('bar2_value', npcHealthFinal)
+      token.set('bar2_max', npcMaxHealth)
+      token.set('bar2_value', npcHealthFinal)
     } else {
       // Update character attributes
       attObjArray[0].setWithWorker('max', npcMaxHealth)
       attObjArray[0].setWithWorker('current', npcHealthFinal)
     }
-    tokenObj.set('status_dead', (npcHealthFinal === 0))
+    token.set('status_dead', (npcHealthFinal === 0))
     if (dmgDealt > 0) {
       sendChat('GM', `/w gm ${npcName} takes ${dmg} damage (${dmgDealt} - ${npcArmor} Armor). Health: ${npcHealth}->${npcHealthFinal}.`)
     } else {
