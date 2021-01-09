51338df3cc8375809e11c10fc89b563ec2a5537c
Mark Stoecker <webmaster@desertwebdesigns.com>
2021-01-09 02:17:34 -0700

    changed script to use R20 API best practices, updated method for retrieving and checking character control

--- treatwounds.js
+++ treatwounds.js
@@ -1,36 +1,75 @@
 /*
 PF2E Treat Wounds Check
 
-Version     0.1
+Version     0.2
 Author:     Mark Stoecker
 Roll20:     https://app.roll20.net/users/580967/mark-s
 BitBucket:  https://bitbucket.org/desertwebdesigns/roll20/src/master/TreatWounds/
 
 */
 
-// Do damage on crit failure
 // Convert to await/async usage to be able to double healing amount on crit success
+// Do damage on crit failure
 // Nat 20 turns failure into success, currently registers as crit success
 
-on("ready", () => {
-    const getCharByName = (charname) => {
-        return findObjs({type: 'character', name: charname});
+// Future versions:
+// Send error message back to chat on failure
+// rename args array in handleInput for ease of use
+// Allow for generic tokens that don't represent a character (requires sending healing modifier with api call)
+
+var RLRGaming = RLRGaming || {};
+
+RLRGaming.TreatWounds = RLRGaming.TreatWounds || (function() {
+    'use strict';
+    
+    const version = "0.2";
+    
+    const getChar = (tokenorname) => {
+        var character = getCharByToken(tokenorname);
+        
+        switch (character) {
+            case undefined:
+                return false;
+                break;
+            case false:
+                character = getCharByName(tokenorname);
+                if (!character) {
+                    return false;
+                }
+                break;
+        }
+        
+        return character;
     };
     
-    const getTokenControl = (args) => {
-        token = getObj("graphic", args[1]);
-        if (token === undefined) {
-            character = getCharByName(args[1]);
-            if (character.length == 1)
-                character = character[0];
-            else
-                return false;
-        } else {
-            tokencontrol = token.get('controlledby').split(",");
-            character = getObj("character", token.get('represents'));
+    const getCharByName = (charname) => {
+        var chars = findObjs({type: 'character', name: charname});
+        if (chars.length == 1)
+            return chars[0];
+        else
+            return false;
+    };
+    
+    const getCharByToken = (tokenid) => {
+        var token = getObj("graphic", tokenid);
+        if (token === undefined)
+            return false;
+        else
+            return getObj("character", token.get('represents'));
+    };
+
+    const hasCharacterControl = (playerid, character) => {
+        if (!character) {
+            log("A valid character name/token was not supplied");
+            return false;
         }
-        charactercontrol = character.get('controlledby').split(",");
-        if ((token !== undefined && _.contains(tokencontrol,args[0])) || _.contains(charactercontrol,args[0]))
+        
+        var charactercontrol = character.get('controlledby').split(",");
+        
+        if (
+            _.contains(charactercontrol,playerid) ||
+            _.contains(charactercontrol,'all')
+        )
             return true;
         else
             return false;
@@ -43,20 +82,20 @@ on("ready", () => {
 
     const performHeal = (character, surgery, DC, player, target) => {
         if (surgery == "1") {
-            surgeryMod = 2;
-            riskySurgery = " + 2[Risky Surgery]";
+            var surgeryMod = 2;
+            var riskySurgery = " + 2[Risky Surgery]";
         } else {
-            surgeryMod = 0;
-            riskySurgery = '';
+            var surgeryMod = 0;
+            var riskySurgery = '';
         };
         
         sendChat(character, "/roll 1d20 + @{" + character + "|medicine} + " + surgeryMod, function(ops) {
-            data = JSON.parse(ops[0].content);
-            result = data.rolls[0].results[0].v;
-            total = data.total;
+            var data = JSON.parse(ops[0].content);
+            var result = data.rolls[0].results[0].v;
+            var total = data.total;
             
             var checkDC = DC.replace( /^\D+/g, '');
-            let crit = 0;
+            var crit = 0;
 
             if (total >= Number(checkDC)+10) {
                 crit += 1;
@@ -71,36 +110,36 @@ on("ready", () => {
             }
             switch (true) {
                 case (crit > 0):
-                    critmsg = "<br>Critical Success!";
+                    var critmsg = "<br>Critical Success!";
                     break;
                 case (crit < 0):
-                    critmsg = "<br>Critical Failure!";
+                    var critmsg = "<br>Critical Failure!";
                     break;
                 default:
-                    critmsg = "";
+                    var critmsg = "";
             }
             
             if (total >= checkDC) {
                 switch (Number(checkDC)) {
                     case 20:
-                        healmod = 10;
+                        var healmod = 10;
                         break;
                     case 30:
-                        healmod = 20;
+                        var healmod = 20;
                         break;
                     case 40:
-                        healmod = 30;
+                        var healmod = 30;
                         break;
                     default:
-                        healmod = 0;
+                        var healmod = 0;
                         break;
                 };
-                heal = true;
+                var heal = true;
             } else {
-                heal = false;
+                var heal = false;
             }
             
-            let healmsg = "&{template:rolls} {{charactername=" + character + "}} {{header=Treat Wounds Check}} {{subheader=Skill}} {{roll01=[[" + result + riskySurgery + " + [@{" + character + "|medicine_proficiency_display}] (@{" + character + "|medicine})[@{" + character + "|text_modifier}] + (@{" + character + "|query_roll_bonus})[@{" + character + "|text_bonus}]]]}} {{roll01_type=skill}}";
+            var healmsg = "&{template:rolls} {{charactername=" + character + "}} {{header=Treat Wounds Check}} {{subheader=Skill}} {{roll01=[[" + result + riskySurgery + " + [@{" + character + "|medicine_proficiency_display}] (@{" + character + "|medicine})[@{" + character + "|text_modifier}] + (@{" + character + "|query_roll_bonus})[@{" + character + "|text_bonus}]]]}} {{roll01_type=skill}}";
 
             if (crit != 0) {
                 healmsg += "{{roll01_info=" + critmsg + "}} ";
@@ -119,17 +158,26 @@ on("ready", () => {
             }
         });
     };
-
-    on("chat:message", msg => {
+    
+    const handleInput = (msg) => {
+        /* args = [
+            0 => !performsurgery,
+            1 => selected token or character name,
+            2 => 1|0 (perform risky rurgery),
+            3 => DC of check,
+            4 => Player to heal
+        ]
+        */
         if(msg.type == "api") {
-            args = msg.content.split(",");
+            var args = msg.content.split(",");
             switch(args[0].toLowerCase()) {
                 case '!performsurgery':
                     if (args.length == 5) {
-                        if (getTokenControl([msg.playerid, args[1]])) {
+                        if (hasCharacterControl(msg.playerid, getChar(args[1]))) {
+                            var charname = getChar(args[1]).get("name");
                             if (args[2] == 1)
-                                performSurgery(args[1], args[4]);
-                            performHeal(args[1], args[2], args[3], msg.playerid, args[4]);
+                                performSurgery(charname, args[4]);
+                            performHeal(charname, args[2], args[3], msg.playerid, args[4]);
                         }
                     } else {
                         sendChat("GM", "/w GM Incorrect number of parameters sent to '!performsurgery'");
@@ -137,5 +185,19 @@ on("ready", () => {
                     }
             }
         }
-    });
+    };
+    
+    const registerEventHandlers = () => {
+        on('chat:message', handleInput);
+    };
+    
+    return {
+        RegisterEventHandlers: registerEventHandlers
+    };
+}());
+
+
+on('ready', () => {
+    'use strict';
+    RLRGaming.TreatWounds.RegisterEventHandlers();
 });
