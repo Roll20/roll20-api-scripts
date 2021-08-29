2361ed04629569e34a8d691f11e5ca6601ce95ab
keithcurtis1 <keithcurtis@gmail.com>
2021-08-29 13:47:43 -0700

    Update token-action-maker.js

--- Token-Action-Maker/token-action-maker.js
+++ Token-Action-Maker/token-action-maker.js
@@ -1,7 +1,7 @@
 var tokenAction = tokenAction || (function() {
     'use strict';
 
-    var version = '0.3.45',
+    var version = '0.3.5',
         sheetVersion = 'D&D 5th Edition by Roll20',
 
         checkInstall = function() {
