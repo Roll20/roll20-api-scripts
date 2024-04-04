0ccf1940c151c9942c4b724e7fa5e0b7334092db
Villain1nGlasses <85969638+Villain1nGlasses@users.noreply.github.com>
2024-04-04 01:00:46 -0600

    Rolled back some HDE text field additions. Not used and risks double quote problems.

--- HeroSystem6eHeroic_HDImporter/1.2/HeroSystem6eHeroic_HDImporter.js
+++ HeroSystem6eHeroic_HDImporter/1.2/HeroSystem6eHeroic_HDImporter.js
@@ -3644,7 +3644,7 @@
 		let text = '<div style="'+style+'">';
 		//text += makeTitle(script_name + ' Help');
 		text += makeTitle('HD Importer Help');
-		text += '<p>Export a character in <a style="color: orange;" href="https://www.herogames.com/store/product/1-hero-designer/" target="_blank">Hero Designer</a> using the <a style="color: orange;" href="https://github.com/Roll20/roll20-api-scripts/tree/master/HeroSystem6eHeroic_HDImporter/1.0" target="_blank">HeroSystem6eHeroic.hde</a> format.</p>';
+		text += '<p>Export a character in <a style="color: orange;" href="https://www.herogames.com/store/product/1-hero-designer/" target="_blank">Hero Designer</a> using the <a style="color: orange;" href="https://github.com/Roll20/roll20-api-scripts/tree/master/HeroSystem6eHeroic_HDImporter/1.2" target="_blank">HeroSystem6eHeroic.hde</a> format.</p>';
 		text += '<p>Locate and open the exported .txt file in a text editor. Copy its entire contents and paste them into the Roll20 chat window. Hit enter.</p>';
 		text += '<p>For more information see the documentation page in the HDImporter <a style="color: orange;" href="https://github.com/Roll20/roll20-api-scripts/tree/master/HeroSystem6eHeroic_HDImporter" target="_blank">Github</a> repository.</p>';
 		text += '<hr>';
