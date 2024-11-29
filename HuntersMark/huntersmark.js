e08ed905e72681d1e272a7bdc3987c6bad881dfd
G-G-G <38135275+G-G-G@users.noreply.github.com>
2024-11-29 23:47:54 +0000

    Hunters-Mark
    Now supports the ability to mark multiple characters at a tie. Some characters need this.

--- HuntersMark/huntersmark.js
+++ HuntersMark/huntersmark.js
@@ -1,34 +1,51 @@
 /*
     !hunters-mark 
     !hunters-mark add
+    !hunters-mark bard
     !hunters-mark delete
     !hunters-mark help
     !hunters-mark show
     !hunters-mark menu
-    !hunters-mark @{selected|character_id} @{target|token_id}
+    !hunters-mark @{selected|token_id} @{target|token_id}
+        this used to be !hunters-mark @{selected|character_id} @{target|token_id}
 
     add     adds selected character as a new hunter. 
             must have exactly one marker
-    delete  removes currently selected character from hunters list
+    bard    as above, but adds a bard-like character
+    delete  removes the currently selected character from all lists
     help    shows help menu, and description of each feature
     show    shows current hunters in state
     menu    shows the menu of buttons
     
     if none of above
-        assumes arg[1] is a hunter character_id, and arg[2] is target token id.
-        if not, will send a warning and end script.
+        assumes arg[1] is a the acting character's token_id, and arg[2] is the target's token id. 
+            (note: this is a change - earlier versions of the script used character_id for args[1])
+        if not, will send a warning and end the script.
     
 */
+var API_Meta = API_Meta || {};
+API_Meta.Reporter = {
+    offset: Number.MAX_SAFE_INTEGER,
+    lineCount: -1
+}; {
+    try {
+        throw new Error('');
+    } catch (e) {
+        API_Meta.Reporter.offset = (parseInt(e.stack.split(/\n/)[1].replace(/^.*:(\d+):.*$/, '$1'), 10) - (4));
+    }
+}
+
 const HUNTERSMARK = (() => { // eslint-disable-line no-unused-vars
 
     const script_name = 'HUNTERSMARK';
-    const version = '0.3.0';
+    const version = '0.4.0';
     const lastUpdate = 1593500895369;
     
     const tokenName = token => token.get('name') ? token.get('name') : (token.get('_id') ? token.get('_id') : 'Unknown');
     const findHunter = (hunter, hunted = 'hunter') => state.HUNTERSMARK.hunters.findIndex(item => item[hunted] === hunter);
+    const findBard = (hunter, hunted = 'hunter') => state.HUNTERSMARK.bards.findIndex(item => item[hunted] === hunter);
     const getWho = who => who.split(' (GM)')[0];
-    const mark = '&#64;&#123;selected|character_id&#125; &#64;&#123;target|token_id&#125;';
+    const mark = '&#64;&#123;selected|token_id&#125; &#64;&#123;target|token_id&#125;';
     const CSS = {
         container: 'border: 1pt solid green; background-color: white; font-size: 0.9em; border-radius: 10px;',
         table: '<table style="border:0;">',
@@ -46,8 +63,14 @@ const HUNTERSMARK = (() => { // eslint-disable-line no-unused-vars
         if(!state.hasOwnProperty('HUNTERSMARK')) {
             state.HUNTERSMARK = {
                 schema: 0.0,
-                hunters: []
+                hunters: [],
+                
             };
+        } else {
+            if(!state.HUNTERSMARK.bards) {
+                state.HUNTERSMARK.bards = [];
+                state.HUNTERSMARK.schema = 0.1;
+            }
         }
         /* hunter: {
             hunter: id of character,
@@ -74,8 +97,10 @@ const HUNTERSMARK = (() => { // eslint-disable-line no-unused-vars
 
         if(!command || command.toLowerCase() === 'help') {
             showHelp(getWho(msg.who));
-        } else if(command.toLowerCase() === 'add') {
+        } else if(command.toLowerCase() === 'add'  || command.toLowerCase() === 'hunter') {
             hunter(msg, 1);
+        } else if(command.toLowerCase() === 'bard') {
+            hunter(msg, 2);
         } else if(command.toLowerCase() === 'delete') {
             hunter(msg, -1);
         } else if(command.toLowerCase() === 'show') {
@@ -89,18 +114,31 @@ const HUNTERSMARK = (() => { // eslint-disable-line no-unused-vars
                 sendChat(script_name,`/w "${getWho(msg.who)}" You must have only one token selected.`);
                 return;
             }
-            tokenMarker(args[1], args[2], getWho(msg.who));
+            const target_id = args[2];
+            const token_id = args[1];
+            let character_id;
+            try {
+                const token = getObj('graphic', token_id);
+                const rep = token.get('represents');
+                const character = getObj('character', rep);
+                character_id = character.id; 
+            } catch(e) {
+                sendChat(script_name,`/w "${getWho(msg.who)}" Check the character's token - is the REPRESENTS field assigned?`);
+                return;
+            }
+            tokenMarker(character_id, token_id, target_id, getWho(msg.who));
         }
     };
 
     const showHelp = who => {
         const help = {
-            show: 'This shows the current list of hunters, and their marks.',
-            add: 'To add a new hunter, select a token representing the character and apply the status marker you want to use as their mark. Then click Add.',
-            'delete': 'To remove a character from the list of hunters, select a token representing them and click Delete.',
+            show: 'This shows the current list of activated tokens, and their marks.',
+            hunter: 'To define a new hunter-like character, select a token representing the character and apply the status marker you want to use as their mark. Then click Hunter.',
+            bard: 'To define a new bard-like character, select a token representing the character and apply the status marker you want to use as their mark. Then click Bard.',
+            'delete': 'To remove a character from their current list, select a token representing them and click Delete.',
             help: 'Show this description.',
-            menu: "Show a set of buttons to activate the script's features.",
-            'mark a target': `<p>To mark a target, use <code>!hunters-mark [character id of hunter] [token id of target]. </code></p><p>A good way to do this is <code>!hunters-mark ${mark}</code></p>`
+            menu: "Displays a button to activate each of the script's features. For convenience, this menu is always shown after Help and Show.",
+            'mark a target': `<p>To mark a target, use <code>!hunters-mark [token id of hunter] [token id of target]. </code></p><p>For example: <code>!hunters-mark ${mark}</code></p>`
         };
         let output = `<div style="${CSS.container}"><h3 style="${CSS.heading}">Hunter's Mark Instructions</h3><p3>Use <code>!hunters-mark</code> followed by one of the commands below.</p>${CSS.table}`;
         Object.entries(help).forEach(([key, value]) => {
@@ -114,7 +152,8 @@ const HUNTERSMARK = (() => { // eslint-disable-line no-unused-vars
     const showMenu = (who) => {
         const buttons = {
             Show: 'show',
-            Add: 'add',
+            Hunter: 'add',
+            Bard: 'bard',
             'Delete': 'delete',
             Help: 'help'
             
@@ -132,8 +171,12 @@ const HUNTERSMARK = (() => { // eslint-disable-line no-unused-vars
     const showState = (who) => {
         const tokenMarkers = JSON.parse(Campaign().get('token_markers'));
         const getIcon = tag => tokenMarkers.find(item => tag === item.tag).url;
-        const hunters = state.HUNTERSMARK.hunters.map(hunter => `<tr><td style="${CSS.leftpad}"><img src="${getIcon(hunter.mark)}"></td><td style="${CSS.leftpad}"><p>**${getObj('character', hunter.hunter).get('name')}**${hunter.marked ? ` </p><p>Marked: ${getObj('graphic',hunter.marked).get('name')}` : ''}</p></td></tr>`);
-        sendChat(script_name, `/w "${who}" <div style="${CSS.container}"><h3 style="${CSS.heading}">Hunter Details</h3><table>${hunters.join('')}</table> </div>`);
+        //const hunters = state.HUNTERSMARK.hunters.map(hunter => `<tr><td style="${CSS.leftpad}"><img src="${getIcon(hunter.mark)}"></td><td style="${CSS.leftpad}"><p>**${getObj('character', hunter.hunter).get('name')}**${hunter.marked ? ` </p><p>Marked: ${getObj('graphic',hunter.marked).get('name')}` : ''}</p></td></tr>`);
+        
+        const hunters = state.HUNTERSMARK.hunters.map(hunter => `<tr><td style="${CSS.leftpad}"><img src="${getIcon(hunter.mark)}"></td><td style="${CSS.leftpad}"><p>**${getObj('character', hunter.hunter).get('name')}**</p></td></tr>`);
+        const bards = state.HUNTERSMARK.bards.map(hunter => `<tr><td style="${CSS.leftpad}"><img src="${getIcon(hunter.mark)}"></td><td style="${CSS.leftpad}"><p>**${getObj('character', hunter.hunter).get('name')}**</p></td></tr>`);
+        sendChat(script_name, `/w "${who}" <div style="${CSS.container}"><h3 style="${CSS.heading}">Hunter-like Characters</h3><p style="text-align: center;">Only one target can be marked.</p><table>${hunters.join('')}</table><h3 style="${CSS.heading}">Bard-like Characters</h3><p  style="text-align: center;">Multiple targets can be marked.</p><table>${bards.join('')}</table> </div>`);
+        showMenu(who);
     };
     
     const hunter = (msg, addordelete) => {
@@ -148,16 +191,26 @@ const HUNTERSMARK = (() => { // eslint-disable-line no-unused-vars
             if (token) {
                 let character = getObj('character', token.get('represents'));
                 if (character) {
+                    const found_hunter = findHunter(character.get('_id'));
+                    const found_bard = findBard(character.get('_id'));                    
                     if(addordelete === -1) {
                         // delete selected characters from state
-                        const found = findHunter(character.get('_id'));
-                        if(found === -1) {
+                        if(found_hunter === -1) {
                             excluded.push(tokenName(token));
                         } else {
-                            state.HUNTERSMARK.hunters.splice(found, 1);
+                            state.HUNTERSMARK.hunters.splice(found_hunter, 1);
                         }
-                    } else if (addordelete === 1) {
+                        
+                        if(found_bard === -1) {
+                            excluded.push(tokenName(token));
+                        } else {
+                            state.HUNTERSMARK.bards.splice(found_bard, 1);
+                        }
+                    } else if (addordelete >= 1) {
                         // only need to check marker if adding.
+
+                        const colour_markers = ["red", "blue", "green", "brown", "purple", "pink", "yellow", "dead"];
+
                         const marker = token.get('statusmarkers').split(',');
                         if(marker.length === 0 || marker.length > 1 || marker[0] === '') {
                             excluded.push(tokenName(token));    
@@ -167,14 +220,23 @@ const HUNTERSMARK = (() => { // eslint-disable-line no-unused-vars
                                 marked: '',
                                 mark: marker[0]
                             };
-                            const found = findHunter(newHunter.hunter);
-                            if(found === -1) {
+                            //problem with coloured status markers - they cause sandbox to crash
+                            if (colour_markers.includes(newHunter.mark)) {
+                                sendChat(script_name,`You cannot use the pure color or dead markers (the top row).`);
+                                return;
+                            }
+                            
+                            // need to delete the token if it already exists, then add it again.
+                            if(found_hunter > -1) state.HUNTERSMARK.hunters.splice(found_hunter, 1)
+                            if(found_bard > -1) state.HUNTERSMARK.bards.splice(found_bard, 1);
+                            if(addordelete == 2) {
+                                state.HUNTERSMARK.bards.push(newHunter);
+                            } else if (addordelete == 1) {
                                 state.HUNTERSMARK.hunters.push(newHunter);
-                            } else {
-                                state.HUNTERSMARK.hunters.splice(found, 1, newHunter);
                             }
                         }
-                    }
+                    } 
+                    
                     showstate = true;
                     // report characters in state.
                 } else {
@@ -192,52 +254,39 @@ const HUNTERSMARK = (() => { // eslint-disable-line no-unused-vars
         }
     };
     
-    const tokenMarker = (hunter_id, target_id, who) => {
-        const hunter_index = findHunter(hunter_id);
-        if(hunter_index === -1) {
-            sendChat(script_name, `/w "${who}" Hunter is not found. Check they are set up properly.`);
+    const tokenMarker = (character_id, token_id, target_id, who) => {
+        const hunter_index = findHunter(character_id);
+        const bard_index = findBard(character_id);
+        if(hunter_index === -1 && bard_index === -1) {
+            sendChat(script_name, `/w "${who}" Marker is not found. Check they are set up properly.`);
             return;
         }
-        const token = getObj('graphic', target_id);
-        if(!token) {
+        const target_token = getObj('graphic', target_id);
+        if(!target_token) {
             sendChat(script_name, `/w "${who}" Target token is not a valid target.`);
             return;
         }
-        /* here starts the actual work of the script */
-        if(target_id == state.HUNTERSMARK.hunters[hunter_index].marked) {
-            // the target token matches the id stored in owner. 
-            // This character is already marked, so unmark him and clear mark_id 
-            state.HUNTERSMARK.hunters[hunter_index].marked = '';
-            changeMarker(target_id, state.HUNTERSMARK.hunters[hunter_index].mark, 'remove');
-        } else {
-            // marking a new target so:
-            // get old mark, and remove mark from previous character
-            // update mark_id and add marker
-            const oldmark = state.HUNTERSMARK.hunters[hunter_index].marked;
-            if(oldmark !== '') {
-                // find old character, remove mark from them, then:
-                changeMarker(oldmark, state.HUNTERSMARK.hunters[hunter_index].mark, 'remove');
-            }
-            state.HUNTERSMARK.hunters[hunter_index].marked = target_id;
-            changeMarker(target_id, state.HUNTERSMARK.hunters[hunter_index].mark, 'add');
-            
+        
+        const mark = (hunter_index > -1) ? state.HUNTERSMARK.hunters[hunter_index].mark : state.HUNTERSMARK.bards[bard_index].mark;
+        // ERROR: want to handle if undefined
+        if(hunter_index > -1) {
+            removeMarkers(mark, target_id, token_id);
         }
+        updateMarker(target_id, mark);
     };
 
-    const changeMarker = (tid, marker, addorremove = 'add') => {
+    const updateMarker = (tid, marker) => {
         const token = getObj('graphic', tid);
         if(token) {
             let tokenMarkers = token.get('statusmarkers').split(',');
-            if(addorremove === 'add') {
-                if(!tokenMarkers.includes(marker)) {
-                    tokenMarkers.push(marker);    
-                }
-            } else if(addorremove === 'remove') {
+            //sendChat('', `marker: ${marker}; markers: ${tokenMarkers}`)
+            if(!tokenMarkers.includes(marker)) {
+                tokenMarkers.push(marker);    
+            } else  {
                 tokenMarkers = tokenMarkers.filter(item => item !== marker);    
-            } else {
-                return;
-            }
+            } 
             token.set('statusmarkers', tokenMarkers.join(','));
+            
         }
     };
     
@@ -250,4 +299,40 @@ const HUNTERSMARK = (() => { // eslint-disable-line no-unused-vars
         registerEventHandlers();
     });
 
+    const removeMarkers = (marker, ...token_ids) => {
+        // tokens is a rest array - it will be an array of tokens to ignore with this function
+        let tokens = findObjs({_subtype: "token"});
+        // want to loop thtrough every token t
+        tokens.forEach(token => {
+            const tid = token.id;
+            if(!token_ids.includes(tid)) {
+                let tokenMarkers = token.get('statusmarkers').split(',');
+                if(tokenMarkers.includes(marker)) {
+                    tokenMarkers = tokenMarkers.filter(item => item !== marker);    
+                    token.set('statusmarkers', tokenMarkers.join(','));
+                } 
+            }
+        });
+    }
 })();
+/* CHANGES
+    make sure to use token_id at the start in place of character_id
+    Removes a crash when selecting any of the first row markers (coloutred and dead).
+    Adds bard-like characters that can assign marks to multiple characters.
+    Can change the assigned marker just by adding them again (before, you had to delete then re-add)
+    Can swap characters between lists just by adding them to relevant list
+    Can't add the same character multiple times to the same list.
+    The way last-marked character is recorded has changed, so it isn't displayted in the Show list any more.
+
+
+    Might want clear marks: 
+        clear all tokens of all marks. (Clear All)
+        clear a single token of all marks (use target). (Clear Token)
+        Remove a specific mark from all tokens. (clear Mark) select a token with one or mor marks, and remove them from all tokens.
+        if a character can mark, add that on to their token (Show Mark). Marke select multiple tokens - could be useful before Clear Mark
+        these would be different functions. Culd add another row of buttons.
+    
+    What if a character has a token-marker that doesn't exist? For example, it was created with a custom set in one game, and that was changed.
+
+    Show function: when displaying chsraacters, maybe don't show a list if it has no characters. If no characters in both, have a boilerplate message.
+*/
